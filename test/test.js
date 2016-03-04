ba18ef662f8bbb410d2b4bd9fc17763e89fba3fa
i327406 <tien.viet.nguyen@sap.com>
2016-03-04 17:37:54 +0700

    some example regards to unit test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,34 @@
 function assert(value, description) {
     if (!value) {
-        throw new Error(description + "is Failed !");
+        console.log(description + " is Failed !");
+    } else {
+        console.log(description + " is Passed !");
     }
 }
 
 assert(5 + 6 === 12, "5 + 6 is 11!");
 
-var sum - function(a, b) {
+var sum = function(a, b) {
     return a + b;
 }
+
+function testSum() {
+    assert(sum(9, 10) === 19, "9 + 10 is 19");
+}
+
+testSum();
+
+
+//test needs wait until 'later', for example: 5 seconds
+var delay = function(func, delayInSeconds) {
+    setTimeout(func, delayInSeconds * 1000);
+}
+
+function testDelay() {
+    var now = new Date().getTime() / 1000;
+    delay(function() {
+        assert(new Date().getTime() / 1000 - now === 5, "delaying 5 seconds");
+    }, 5);
+}
+
+testDelay();
