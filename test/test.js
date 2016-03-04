acb242218295c6b66e624eaaf73f40925e8a96a6
i327406 <tien.viet.nguyen@sap.com>
2016-03-04 11:31:38 +0700

    init a product for learning openui5 and QUnit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+function assert(value, description) {
+    if (!value) {
+        throw new Error(description + "is Failed !");
+    }
+}
+
+assert(5 + 6 === 12, "5 + 6 is 11!");
+
+var sum - function(a, b) {
+    return a + b;
+}
